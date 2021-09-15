d24c620e92f735690e4900952f62c406665aa4e0
James Chandler <stillsound@protonmail.com>
2021-09-15 10:18:50 -0700

    create update comment route

--- server/routes/comments.js
+++ server/routes/comments.js
@@ -1,19 +1,38 @@
 const router = require("express").Router();
 const User = require("../models/user");
-const Comment = require("../models/comment")
-const Playlist = require("../models/Playlist")
+const Comment = require("../models/comment");
+const Playlist = require("../models/Playlist");
 const { requireLogin } = require("../middleware/auth");
 
+router.post("/auth", requireLogin, isLoggedIn, async (req, res) => {
+  Playlist.findById(req.params.id).then((foundPlaylist) => {
+    if (!foundPlaylist)
+      return res.status(404).json({ message: "Playlist not found" });
+    foundPlaylist.comment.push(req.body);
+    foundPlaylist.save();
+    return res.status(201).json({
+      message: "Successfully made comment",
+      data: foundPlaylist.comment,
+    });
+  });
+  try {
+  } catch (err) {
+    console.log(err);
+  }
+});
 
-router.post('/auth', requireLogin, isLoggedIn, async (req, res) => {
-    Playlist.findById(req.params.id)
-    .then((foundPlaylist) => {
-        if (!foundPlaylist) return res.status(404).json({ message: "Playlist not found"});
-        foundPlaylist.
-    })
-    try {
-        
-    } catch (err) {
-        console.log(err)
+router.put("/auth", requireLogin, isLoggedIn, async (req, res) => {
+  Comment.findById(
+    req.params.id,
+    req.body,
+    { new: true },
+    (err, updatedComment) => {
+      console.log(updatedComment);
+      if (err) console.log("Error in updating comment", err);
+      return res.status(202).json({
+        message: "Successfully updated comment",
+        data: updatedComment,
+      });
     }
-})
+  );
+});

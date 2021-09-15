176e16f901803475e21b1bcd5cfae9de315530a6
James Chandler <stillsound@protonmail.com>
2021-09-14 18:51:01 -0700

    double auth work now!

--- server/server.js
+++ server/server.js
@@ -4,43 +4,43 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const path = require("path");
 const routes = require("./routes");
-const cookieSession = require("cookie-session");
+// const cookieSession = require("cookie-session");
 const app = express();
-const passport = require("passport");
-const SpotifyStrategy = require("passport-spotify").Strategy;
+// const passport = require("passport");
+// const SpotifyStrategy = require("passport-spotify").Strategy;
 
-app.use(
-  cookieSession({
-    name: "spotify-auth-session",
-    keys: ["key1", "key2"],
-  })
-);
+// app.use(
+//   cookieSession({
+//     name: "spotify-auth-session",
+//     keys: ["key1", "key2"],
+//   })
+// );
 
-passport.serializeUser(function (user, done) {
-  done(null, user);
-});
-passport.deserializeUser(function (user, done) {
-  done(null, user);
-});
-let url = "http://localhost:5000/auth/spotify/callback";
+// passport.serializeUser(function (user, done) {
+//   done(null, user);
+// });
+// passport.deserializeUser(function (user, done) {
+//   done(null, user);
+// });
+// let url = "http://localhost:5000/auth/spotify/callback";
 
-const client_id = "cb4e46622559403d9acdba7144bf4831";
-const client_secret = "cd315e345f264902bd1406a642bb2830";
+// const client_id = "cb4e46622559403d9acdba7144bf4831";
+// const client_secret = "cd315e345f264902bd1406a642bb2830";
 
-passport.use(
-  new SpotifyStrategy(
-    {
-      clientID: client_id,
-      clientSecret: client_secret,
-      callbackURL: "http://localhost:3000/auth",
-    },
-    function (accessToken, refreshToken, expires_in, profile, done) {
-      User.findOrCreate({ spotifyId: profile.id }, function (err, user) {
-        return done(err, user);
-      });
-    }
-  )
-);
+// passport.use(
+//   new SpotifyStrategy(
+//     {
+//       clientID: client_id,
+//       clientSecret: client_secret,
+//       callbackURL: "http://localhost:3000/auth",
+//     },
+//     function (accessToken, refreshToken, expires_in, profile, done) {
+//       User.findOrCreate({ spotifyId: profile.id }, function (err, user) {
+//         return done(err, user);
+//       });
+//     }
+//   )
+// );
 
 
 
@@ -60,8 +60,8 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "..", "build")));
 app.use(express.static("public"));
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(passport.initialize());
+// app.use(passport.session());
 
 // app.use((req, res, next) => {
 //   console.log(req.url);
@@ -84,25 +84,25 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-app.get("/auth", (req, res) => {
-  res.send("Hello world");
-});
+// app.get("/auth", (req, res) => {
+//   res.send("Hello world");
+// });
 
-app.get("/auth/auth/error", (req, res) => res.send("Unknown Error"));
-app.get("/auth/auth/spotify", passport.authenticate("spotify"));
-app.get(
-  "/auth/auth/spotify/callback",
-  passport.authenticate("spotify", {
-    failureRedirect: "/auth/auth/login",
-    scope: [
-      "user-read-email", 
-      "user-read-private",
-    ],
-  }),
-  function (req, res) {
-    res.redirect("/auth");
-  }
-);
-app.get
+// app.get("/auth/auth/error", (req, res) => res.send("Unknown Error"));
+// app.get("/auth/auth/spotify", passport.authenticate("spotify"));
+// app.get(
+//   "/auth/auth/spotify/callback",
+//   passport.authenticate("spotify", {
+//     failureRedirect: "/auth/auth/login",
+//     scope: [
+//       "user-read-email", 
+//       "user-read-private",
+//     ],
+//   }),
+//   function (req, res) {
+//     res.redirect("/auth");
+//   }
+// );
+// app.get
   
 app.listen(PORT, () => console.log("Server is running"));

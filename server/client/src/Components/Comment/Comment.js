bbfe0deae0d04a89c50d3c5659f6cf6d2318eb43
James Chandler <stillsound@protonmail.com>
2021-09-14 22:32:02 -0700

    comment component

---
+++ server/client/src/Components/Comment/Comment.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+import * as UserService from "../../api/UserService";
+
+const user = UserService.getUser()
+
+function Comment() {
+    const [body, setBody] = useState('')
+
+    const handleSubmit = async () => {
+        const res = await user;
+        if (res.data) {
+            setBody('')
+        } else {
+            alert('STOP, ERROR --- YOU CANT DO THAT')
+        }
+
+    }
+    return (
+      <div>
+        <input
+          type="text"
+          className="comment-form"
+          onChange={(e) => setBody(e.target.value)}
+          value={body}
+        />
+        <button onClick={handleSubmit}>LOGIN</button>
+      </div>
+    );
+}
+
+export default Comment

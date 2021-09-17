22ec528cd6a71e131b8f379fc0f530be08ecd8e6
James Chandler <stillsound@protonmail.com>
2021-09-16 23:35:46 -0700

    working on getting post to show up

--- server/client/src/Components/Post/Post.js
+++ server/client/src/Components/Post/Post.js
@@ -2,52 +2,60 @@ import React, { useState, useEffect } from 'react';
 import { func, string, array } from "prop-types";
 import * as PostService from "../../api/PostService";
 
+
+const getPosts = async () => {
+}
+
 function Post({ id,  body,  user }) {
-      const [post, setPost] = useState([]);
-      const [isEditing, setIsEditing] = useState(false);
-      const [editedBody, setBody] = useState(body); 
-      
-      async function fetchPosts() {
-        let res = await PostService.getAllPosts();
-        console.log("POST SERVICE RESPoNSE", res.data)
-        if (res.status === 200) {
-          const revData = res.data
-          console.log(revData)
-          setPost(revData);
-        }
+  
+  const [post, setPost] = useState([]);
+  const [editedBody, setBody] = useState(body); 
+  const [isEditing, setIsEditing] = useState(false);
+  //this is where we left off
+  let _post = PostService.getAllPosts();
+  
+  console.log("POST SERVICE RESPoNSE", post.data);
+  useEffect(() => {
+    getPosts();
+  }, []);
+  
+  if (post.status === 200) {
+    const revData = post.data;
+    console.log(revData);
+    setPost(revData);
+    return (
+      {revData}
+      );
+    }
+    
+    const handleEdit = async () => {
+      console.log("FROM HANDLE EDIT");
+      setIsEditing(!isEditing);
+      if (isEditing) {
+        let editedPost = {
+          body: editedBody,
+        };
+        await PostService.update(id, editedPost);
+        getPosts()
       }
-
-      const handleEdit = async () => {
-        console.log("FROM HANDLE EDIT");
-        setIsEditing(!isEditing);
-        if (isEditing) {
-          let editedPost = {
-            body: editedBody,
-          };
-          await PostService.update(id, editedPost);
-          fetchPosts()
-        }
-      }
-       const handleDelete = async () => {
-         await PostService.remove(id);
-         fetchPosts();
-       };
-
-       useEffect(() => {
-         fetchPosts();
-       }, []);
-
-        return (
-        <div className="comment">
+    }
+    const handleDelete = async () => {
+      await PostService.remove(id);
+      getPosts();
+    };
+    
+    
+    return (
+      <div className="comment">
             <span className="entry">
                 {!isEditing && <span> {body}</span>}
                 {isEditing && (
-                    <input
-                        onChange={(e) => setBody(e.target.value)}
-                        value={editedBody}
-                        type="text"
-                        name="body"
-                        placeholder="BODY GOES HERE"
+                  <input
+                  onChange={(e) => setBody(e.target.value)}
+                  value={editedBody}
+                  type="text"
+                  name="body"
+                  placeholder="BODY GOES HERE"
                     />
                 )}
             </span>
